cf0ca50b859b6a6b487f24188c7167ecd0964e6d
Jong Eun Won <87378210+johnwon2007@users.noreply.github.com>
2024-11-04 17:31:10 +0900

    Update main.py for clear reading

--- main.py
+++ main.py
@@ -3,19 +3,11 @@ from camera import camera_on as co
 from custom_vision import find_drink as fd
 import numpy as np
 
-
-# def show_popup():
-#     return gr.update(visible=True), gr.update(visible=True)
-
-# def hide_popup():
-#     return gr.update(visible=False), gr.update(visible=False)
-
-
 with gr.Blocks() as demo:
     #카메라로 사진찍기 클릭 시 카메라 창 띄워주기
-    with gr.Column(scale=1, min_width=300): #input related fields
+    with gr.Column(scale=1, min_width=300):
+        
         #이미지 업로드 관련 코드
-
         image_input = gr.Image(type="pil", label="Upload an Image", height=300)
         img_upload_button = gr.Button("이미지 업로드")
         output_text = gr.Textbox(label="Beer Name")
