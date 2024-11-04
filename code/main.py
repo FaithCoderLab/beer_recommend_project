c5fc95f54eb124c010a5c28784d45102fcd2204d
John Won <johnwon2007@gmail.com>
2024-11-04 17:43:34 +0900

    reformatted files

---
+++ code/main.py
@@ -0,0 +1,18 @@
+import gradio as gr
+from code.camera import camera_on as co
+from code.custom_vision import find_drink as fd
+import numpy as np
+
+with gr.Blocks() as demo:
+    #카메라로 사진찍기 클릭 시 카메라 창 띄워주기
+    with gr.Column(scale=1, min_width=300):
+        
+        #이미지 업로드 관련 코드
+        image_input = gr.Image(type="pil", label="Upload an Image", height=300)
+        img_upload_button = gr.Button("이미지 업로드")
+        output_text = gr.Textbox(label="Beer Name")
+        img_upload_button.click(fn=fd, inputs=image_input, outputs=output_text)
+        recom_button = gr.Button("비슷한 상품 보기")
+
+if __name__ == "__main__":
+    demo.launch(debug=True, share=True, inline=False, height=600)
